e0ab5d5a789799ff4c1c19f7d73d60ff0297ef15
ChrisMak <cmakride@gmail.com>
2022-06-14 15:39:05 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// Please create a web page that has a 4x4 grid, with a border around it. The grid should be horizontally centered in the web page. Every 250 milliseconds, one of the eligible squares should be randomly selected to change colors. Squares are eligible for a color change if its color has not changed in the last two seconds.
+
+// Feel free to conduct research on the web, but do not ask anyone for help. Also, do not use any libraries (e.g. jQuery) or any publishing software such as Microsoft Word. If you’re unable to create this page (even after consulting the web), this role probably won’t be a good fit. We encourage you to spend some time to verify the logic in your solution.
+
+//html elements
+const grid = document.querySelector("#grid")
+for(let i = 0; i < 16 ; i++){
+  let newDiv = document.createElement('div')
+    newDiv.id = `gridEl-${i}`
+    newDiv.style.width = "100px"
+    newDiv.style.height = "100px"
+    newDiv.style.backgroundColor = "#f5f1dc"
+    grid.appendChild(newDiv)
+}
